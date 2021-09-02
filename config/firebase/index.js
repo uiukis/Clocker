9dd3f7aaa9dcdc4410950f5678a2e833b0fdbba3
uiukis <wilker.sou26@gmail.com>
2021-09-02 19:32:05 -0300

    Initial commit from Create Next App

---
+++ config/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app' 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAk3h91a2FSXwa0deIT1aC5OCHeheAkcl0",
+    authDomain: "clocker-0913.firebaseapp.com",
+    projectId: "clocker-0913",
+    storageBucket: "clocker-0913.appspot.com",
+    messagingSenderId: "618619070270",
+    appId: "1:618619070270:web:9db7576139f82622a82e6f",
+    measurementId: "G-82LJ1KK7SC"
+}
+
+export default firebase.getApps.length
+    ? firebase.app()
+     : firebase.initializeApp(firebaseConfig)
