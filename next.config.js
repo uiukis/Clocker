d337035a57bf85d9e8b3b736c1633eeb8e87b4ea
uiukis <wilker.sou26@gmail.com>
2021-09-22 15:40:39 -0300

    Final Project

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    webpack5: false,
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
--- pages/schedule.js
+++ pages/schedule.js
@@ -1,3 +1,4 @@
+
 export default function Schedule() {
     return (
         <div>
